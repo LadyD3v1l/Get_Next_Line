195727a9b63ecc2875cc18b2a8376783697715e7
LadyD3v1l <melissamarquespt@hotmail.com>
2025-05-07 00:40:13 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,39 @@
+/*int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("arquivo.txt", O_RDONLY);
+	if (fd < 0)
+		return (1);
+
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	printf("%c", '\n');
+	return (0);
+}*/
+// IMPRIME O TEXTO COMPLETO
+
+/*int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("arquivo.txt", O_RDONLY);
+	if (fd < 0)
+		return (1);
+
+	line = get_next_line(fd);
+	if (line)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
+}*/
+// IMPRIME UMA LINHA
