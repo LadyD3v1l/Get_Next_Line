dd799dcc8df328fc1f1bc9fc54f3f7935979c61b
Joana Melissa <melissamarquespt@hotmail.com>
2025-05-09 15:40:25 +0100

    Update main.c

--- main.c
+++ main.c
@@ -28,12 +28,11 @@
 		return (1);
 
 	line = get_next_line(fd);
-	if (line)
-	{
 		printf("%s", line);
 		free(line);
-	}
 	close(fd);
+	get_next_line(-1);
 	return (0);
 }*/
+// IMPRIME UMA LINHA
 // IMPRIME UMA LINHA
